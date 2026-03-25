04b8574c436d67e33010ab131ab0e6307e4a5497
AltingunA <alimurtezaaltingun@hotmail.com>
2026-03-25 18:00:24 +0100

    Add files via upload

---
+++ Bin2Tree/PathNames.h
@@ -0,0 +1,20 @@
+#ifndef PATH_NAMES_DATA_CODE
+#define PATH_NAMES_DATA_CODE 1
+
+// const char*
+const char *DATA_PATH_NAME = "/Users/alialtingun/Lecroy_Binary_Reader/Data/Raw";
+const char *WORK_DIR_NAME =
+    "/Users/alialtingun/Lecroy_Binary_Reader/Data/dataTrees";
+#define WORKDIR "/Users/alialtingun/Lecroy_Binary_Reader"
+#define RUN_TYPE "PRODUCTIONraw"
+
+#define RUNMIN 0
+#define RUNMAX 100
+
+#define RUNMIN2 100
+#define RUNMAX2 9999
+
+#define RUNMIN3 10000
+#define RUNMAX3 999999
+
+#endif
