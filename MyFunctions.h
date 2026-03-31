8b512b3a9c378e4c8711b2bc35d7659b1a984773
AltingunAli <alimurtezaaltingun@hotmail.com>
2026-03-31 13:48:24 +0200

    Debugging Start from beginning: snprintf change

--- MyFunctions.h
+++ MyFunctions.h
@@ -1,35 +1,34 @@
-#include <cstddef>
 #ifndef MYFUNCTIONS_H
 #define MYFUNCTIONS_H 1
 
 #include <TROOT.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <iostream>
 
-#include <cmath>
 #include <fstream>
+#include <cmath>
 #include <iomanip>
 
-#include <TCanvas.h>
-#include <TCutG.h>
-#include <TF1.h>
 #include <TFile.h>
+#include <TH1F.h>
+#include <TPostScript.h>
+#include <TPDF.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TStyle.h>
+#include <TCutG.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TMath.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
-#include <TH1D.h>
-#include <TH1F.h>
-#include <TH2D.h>
-#include <TLatex.h>
-#include <TMath.h>
-#include <TPDF.h>
-#include <TPaveStats.h>
-#include <TPostScript.h>
-#include <TSpline.h>
-#include <TStyle.h>
 #include <TTree.h>
-// //
+#include <TSpline.h>
+#include <TPaveStats.h>
+#include <TLatex.h>
+// // 	
 #define DEBUG 1
 #undef DEBUG
 #define DEBUGMSG 1
@@ -37,103 +36,106 @@
 #define SLOWFILES 1
 // #undef SLOWFILES
 
-typedef struct {
-  //    float corr;
-  double bsl;
-  double rms;
-  double ftime;
-  double stime;
-  double maxtime;
-  double maxtimeS;
-  double time50;
-  double ampl;
-  double sampl;
-  double intg;
-  double charge;
-  double chargeIon;
-  double chi2;
-  double t1;
-  double t2;
-  double t3;
-  double a; /// fit function f = a*x + b
-  double b;
-  double ttrig;
-} DPARAM;
 
 typedef struct {
-  //    float corr;
-  double bsl;
-  double rms;
-  int stime;
-  int ftime;
-  int maxtime;
-  double tot;
-  double ampl;
-  double charge;
-  double risecharge;
-  double t10;
-  double tb10;
-  double t90;
-  double ttrig;
-  double width;
-  double sampl;
-  double fampl;
-  double bslch;
-} IPARAM;
+//    float corr;
+   double bsl;
+   double rms;
+   double ftime;
+   double stime;
+   double maxtime;
+   double maxtimeS;
+   double time50;
+   double ampl;
+   double sampl;
+   double intg;
+   double charge;
+   double chargeIon;
+   double chi2;
+   double t1;
+   double t2;
+   double t3;
+   double a;  /// fit function f = a*x + b
+   double b;
+   double ttrig;
+   } DPARAM;
 
 typedef struct {
-  //    float corr;
-  int npeaks;
-  double *tot;
-  double *ampl;
-  double *charge;
-  double *risecharge;
-  double *t10;
-  double *tb10;
-  double *t90;
-  double *ttrig;
-  double *width;
-  double *sampl;
-  double *fampl;
-  double *bslch;
-} PPARAM;
+//    float corr;
+   double bsl;
+   double rms;
+   int stime;
+   int ftime;
+   int maxtime;
+   double tot;
+   double ampl;
+   double charge;
+   double risecharge;
+   double t10;
+   double tb10;
+   double t90;
+   double ttrig;
+   double width;
+   double sampl;
+   double fampl;
+   double bslch;
+   } IPARAM;
 
 typedef struct {
-  //    float corr;
-  int detNo;
-  int runNo;
-  double ampl;
-  double sampl;
-  double charge;
-  double scharge;
-  double rate;
-  double srate;
-  double grate;
-  double sgrate;
-  double tot;
-  double stot;
-  double risetime;
-  double srisetime;
-  double width;
-  double swidth;
-  double chovampl;
-  double schovampl;
-} RUNPAR;
+//    float corr;
+   int npeaks;
+   double *tot;
+   double *ampl;
+   double *charge;
+   double *risecharge;
+   double *t10;
+   double *tb10;
+   double *t90;
+   double *ttrig;
+   double *width;
+   double *sampl;
+   double *fampl;
+   double *bslch;
+   } PPARAM;
 
+typedef struct {
+//    float corr;
+   int detNo;
+   int runNo;
+   double ampl;
+   double sampl;
+   double charge;
+   double scharge;
+   double rate;
+   double srate;
+   double grate;
+   double sgrate;
+   double tot;
+   double stot;
+   double risetime;
+   double srisetime;
+   double width;
+   double swidth;
+   double chovampl;
+   double schovampl;
+   } RUNPAR;
+
+   
 const double rootConv = 2871763200.;
 const double unixConv = 2082844800.;
 
-const std::string RED("\033[0;31m");
-const std::string RED_U_GBKG("\033[4;31;42m");
-const std::string GREEN("\033[0;32m");
-const std::string YELLOW("\033[0;33m");
-const std::string BLUE("\033[0;34m");
-const std::string MAGENTA("\033[0;35m");
-const std::string CYAN("\033[0;36m");
-const std::string INVERSE_ON("\033[7m");
-const std::string INVERSE_OFF("\033[27m");
-const std::string RESET_COLOR("\033[0m");
-const std::string endlr("\n\033[0m");
+const std::string RED("\033[0;31m"); 
+const std::string RED_U_GBKG("\033[4;31;42m"); 
+const std::string GREEN("\033[0;32m"); 
+const std::string YELLOW("\033[0;33m"); 
+const std::string BLUE("\033[0;34m"); 
+const std::string MAGENTA("\033[0;35m"); 
+const std::string CYAN("\033[0;36m"); 
+const std::string INVERSE_ON("\033[7m"); 
+const std::string INVERSE_OFF("\033[27m"); 
+const std::string RESET_COLOR("\033[0m");	
+const std::string endlr("\n\033[0m");	
+
 
 const int MAX_N_FILES = 11000;
 
@@ -170,22 +172,30 @@ const std::size_t MAXVECSIZE =
 const int FRAMESIZE =
     2E7; /// 20 million samples per event, which is 160 MB per event for 4
          /// channels with double precision. Adjust as needed.
+const int MAXTRIG = 25E6;
 
-inline void replaceEOL(char *fnames) {
-  int nchar = 0;
-  while (fnames[nchar] != '\n') {
-    nchar++;
-  }
-  fnames[nchar] = '\0';
-}
 
-inline void replaceEOLT(char *fnames) {
-  int nchar = 0;
-  while (fnames[nchar] != '\n') {
-    nchar++;
-  }
-  fnames[nchar] = '\t';
-  //     fnames[nchar+1]='\0';
-}
+inline void replaceEOL(char *fnames)
+{
+    int nchar=0;
+    while (fnames[nchar]!='\n')
+    {
+      nchar++;
+    }
+    fnames[nchar]='\0';
+}  
+
+inline void replaceEOLT(char *fnames)
+{
+    int nchar=0;
+    while (fnames[nchar]!='\n')
+    {
+      nchar++;
+    }
+    fnames[nchar]='\t';
+//     fnames[nchar+1]='\0';
+}  
+
+
 
 #endif
